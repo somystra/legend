d7ac0471fa4c33027b8968c189c71620c296bd19
somystra <akmalsomirzaev64@gmail.com>
2026-03-28 21:51:05 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # Xavfsizlik uchun
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
